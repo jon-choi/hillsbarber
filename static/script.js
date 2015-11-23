9796a49c833a6aa290c5ba1a548315e755bb052c
Jon Choi <choi.jonathanh@gmail.com>
2015-11-22 17:16:29 -0800

    routing for nav

--- static/script.js
+++ static/script.js
@@ -13,26 +13,27 @@ hillsbarberApp.config(function($routeProvider) {
 
         // route for home
         .when('/', {
-            templateUrl : 'templates/bootstrap_cover.html',
+            // templateUrl : 'templates/bootstrap_cover.html',
+            templateUrl : '../static/partials/about.html',
             controller  : 'mainController'
         })
 
         // route for rates
-        .when('/#/rates', {
+        .when('/rates', {
             templateUrl : '../static/partials/rates.html',
             controller  : 'ratesController'
         })
 
         // routes for contact
         .when('/contact', {
-            templateUrl : 'contact.html',
+            templateUrl : '../static/partials/contact.html',
             controller  : 'contactController'
         });
 });
 
 hillsbarberApp.controller('mainController', function($scope) {
 
-    $scope.message = 'check out this haircut!';
+    $scope.message = 'check out this main haircut!';
 });
 
 hillsbarberApp.controller('ratesController', function($scope) {
